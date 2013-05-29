8cfded36ad86caece6e2585c6fa6ea4be13bc3ab
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2013-05-29 17:21:29 +0200

    added visibility decoder

--- metar_decoder.R
+++ metar_decoder.R
@@ -16,6 +16,23 @@
 #
 # from page A-27 to A-38
 
+# FEDERAL COORDINATOR
+# FOR
+# METEOROLOGICAL SERVICES AND
+# SUPPORTING RESEARCH
+# 8455 COLESVILLE ROAD, SUITE 1500
+# SILVER SPRING, MARYLAND 20910
+# 301-427-2002
+# www.ofcm.gov
+# FEDERAL METEOROLOGICAL HANDBOOK
+# NUMBER 1
+# SURFACE WEATHER OBSERVATIONS AND REPORTS
+# FCM-H1-2005
+# Washington, D.C.
+# September 2005
+# http://www.ofcm.gov/fmh-1/pdf/FMH1.pdf (accessed 20130529)
+
+
 # Some METAR samples:
 
 # http://www.wunderground.com/metarFAQ.asp
@@ -24,18 +41,55 @@ wu = "METAR  KORD	041656Z	19020G26KT	6SM	-SHRA	BKN070	12/08	A3016	RMK AO2"
 # http://aviationweather.gov/adds/metars/
 lipe = "LIPE 282020Z 13006KT 9999 FEW080 15/13 Q1004"
 live = "LIVE 281955Z 16005KT 9999 BKN030 08/05 Q1004 RMK BKN VIS MIN 9999"
+live_1 = "LIVE 291055Z 02006KT 3000 -RA BR SCT010 BKN020 03/M01 Q0999 RMK OVC VIS MIN 3000"
 birk = "BIRK 282000Z 17009KT 9999 FEW032 SCT050 10/04 Q1009"
 llbg = "LLBG 282020Z VRB03KT CAVOK 22/16 Q1016 NOSIG"
 licr = "LICR 281950Z 02016KT 9999 FEW035 20/13 Q1004 RMK VIS MIN 9999"
 kslc = "KSLC 282040Z 29006KT 5SM +RA BR SCT011 BKN020 OVC041 12/10 A2983 RMK AO2 P0012"
 eidw = "EIDW 282030Z 35007KT 9999 FEW015 BKN100 09/07 Q1004 NOSIG"
+paed = "PAED 291155Z AUTO 00000KT 10SM CLR 11/06 A2966 RMK AO2 SLP045 T01090057 10184 20105 51004"
+klxv = "KLXV 291130Z AUTO 07005KT 1/4SM +SN FG VV010 00/M01 A2991 RMK AO2 P0002 FZRANO"
+klxv_1 = "KLXV 291114Z AUTO 36003KT 3/4SM -SN BKN019 OVC035 01/M02 A2992 RMK AO2 P0000 FZRANO"
+klxv_2 = "KLXV 291110Z AUTO 33004KT 2 1/2SM -SN BKN024 OVC035 01/M02 A2991 RMK AO2 P0000"
+kccu = "KCCU 291112Z AUTO 24009KT 180V280 1 3/4SM -SN BKN002 BKN006 OVC011 01/M01 A2998 RMK AO2 LTG DSNT NE"
+
+FMH_table_12_1_visibility = c("M1/4","1/4","1/2","3/4","1","1 1/4","1 1/2","1 3/4","2","2 1/2","3","4","5","6","7","8","9","10",
+                              "0","1/16","1/8","3/16","1/4","5/16","3/8","1/2","5/8","3/4","7/8","1","1 1/8","1 1/4","1 3/8","1 1/2","1 5/8","1 3/4","1 7/8","2","2 1/4","2 1/2","2 3/4","3","4","5","6","7","8","9","10","11","12","13","14","15","20","25","30","35")
 
 
 
 ICAO_station_length = 4
 YYGGggZ_length = 7
 wind_variation_lenght = 7
+WMO_visibility_length = 4
 
+
+
+substrRight = function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+substrLeft = function(x, n){
+  substr(x, 1, nchar(x)-n)
+}
+
+fix_white_space_in_FMH_visibility = function(metar_string)
+{
+  re1="\\d";  # Any Single Digit 1
+  re2="(\\s+)";  # White Space 1
+  re3="\\d";	# Any Single Digit 2
+  re4="\\/";	# Any Single Character 1
+  re5="\\d";	# Any Single Digit 3
+  re6="S";	# Any Single Word Character (Not Whitespace) 1
+  re7="M";	# Any Single Word Character (Not Whitespace) 2
+  re = paste(re1,re2,re3,re4,re5,re6,re7,sep="")
+  result = regexec(re,metar_string)
+  if ( result[[1]][[1]] != -1 ) {
+    white_space_position = result[[1]][[2]]
+    substr(metar_string,white_space_position,white_space_position) = '+'
+  }
+  metar_string
+}
 
 extract_ICAO_location_indicator = function(CCCC)
 {
@@ -111,8 +165,9 @@ extract_wind_info = function(dddffGfmfm)
 }
 
 
-metar_decoder = function(metar_string)
+metar_decoder = function(metar_string,low_visibility=1/32)
 {
+  metar_string = fix_white_space_in_FMH_visibility(metar_string)
   groups = scan(what=character(),text=metar_string)
   
   next_index = 0
@@ -198,29 +253,51 @@ metar_decoder = function(metar_string)
   WIND_DIRECTION_VARIATION = F
   extreme_wind_direction_n = NA
   extreme_wind_direction_x = NA
+  visibility = NA
+  visibility_UOM = NA
+  CAVOK = F
   if (nchar(groups[next_index])==wind_variation_lenght && substr(groups[next_index],4,4)=='V'){
     WIND_DIRECTION_VARIATION = T
     extreme_wind_direction_n = as.numeric(substr(groups[next_index],1,3))
     extreme_wind_direction_x = as.numeric(substr(groups[next_index],5,7))
+    next_index = next_index + 1
+  } 
+  
+  if ( suppressWarnings(!is.na(as.numeric(groups[next_index]))) && nchar(groups[next_index])==WMO_visibility_length ){
+    visibility = as.numeric(groups[next_index])
+    visibility_UOM = "meter"
+  } else if (substrRight(groups[next_index],2)=="SM") {
+    visibility_UOM = "SM"
+    if(substr(groups[next_index],1,1)=='M'){
+      visibility = low_visibility
+    } else {
+      visibility = eval(parse(text=(substrLeft(groups[next_index],2))))
+    }
+  } else if (groups[next_index]=="CAVOK") {
+    CAVOK = T
   } else {
-    WIND_DIRECTION_VARIATION = F
-    extreme_wind_direction_n = NA
-    extreme_wind_direction_x = NA
-    # todo: 15.6
+    stop(paste("Expected visibility group, found",groups[next_index]))
   }
+  
   
   data.frame(METAR,SPECI,COR,ICAO_location_indicator,
              day,hour,minute,
              NIL,AUTO,
              CALM,VRB,direction,speed,GUST,gust_speed,wind_speed_UOM,
-             WIND_DIRECTION_VARIATION,extreme_wind_direction_n,extreme_wind_direction_x)
+             WIND_DIRECTION_VARIATION,extreme_wind_direction_n,extreme_wind_direction_x,CAVOK,visibility,visibility_UOM)
 }
 
 print(metar_decoder(wu))
 print(metar_decoder(lipe))
 print(metar_decoder(live))
+print(metar_decoder(live_1))
 print(metar_decoder(birk))
 print(metar_decoder(llbg))
 print(metar_decoder(licr))
 print(metar_decoder(kslc))
 print(metar_decoder(eidw))
+print(metar_decoder(paed))
+print(metar_decoder(klxv))
+print(metar_decoder(klxv_1))
+print(metar_decoder(klxv_2))
+print(metar_decoder(kccu))
